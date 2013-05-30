0478cf2d6a017209ee98b156ffd58fed36555b0a
Geoff Schorkopf <gschorkopf@gmail.com>
2013-05-29 19:35:43 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eldoorado::Application.config.secret_token = '69b1de5cef7d0090ba9ad52ab9948f9b72bc3daac506c9256ac8a28aec95087110ce88f309367e3f6f85751174cc5ff536fbf779648047d66906450633e17f6e'
